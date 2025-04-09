fa2a6cc82c15b65afd6475a57235761ca8faf869
kimiya85 <kimiyabarmaki@gmail.com>
2025-04-09 11:15:58 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication14
+namespace ConsoleApplication8
 {
     class Program
     {
     }
-namespace مضارب_5_بین_دو_عدد
+
+namespace اختلاف_سنی
     {
         internal class Program
         {
             static void Main(string[] args)
             {
-                int a, b, i;
-                Console.WriteLine("ple enter number 1 : ");
-                a = int.Parse(Console.ReadLine());
-                Console.WriteLine("ple enter number 2 : ");
-                b = int.Parse(Console.ReadLine());
-                for (i = a + 1; i <= b - 1; i++)
-                    if (i % 5 == 0)
-                        Console.WriteLine("result : " + i);
+                int y1, m1, d1, y2, m2, d2, r, r1, r2, r3;
+                Console.WriteLine("ple enter friest year birth : ");
+                y1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("ple enter friest year birth : ");
+                m1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("ple enter friest year birth : ");
+                d1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("ple enter friest year birth : ");
+                y2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("ple enter friest year birth : ");
+                m2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("ple enter friest year birth : ");
+                d2 = int.Parse(Console.ReadLine());
+                if (m1 <= 6)
+                {
+                    r1 = 365 - (m1 - 1) * 31 + d1;
+                }
+                else
+                {
+                    r1 = 365 - (186 + ((m1 - 7) * 30) + d1);
+                }
+                if (m2 <= 6)
+                {
+                    r2 = 365 - (m2 - 1) * 31 + d2;
+
+                }
+                else
+                {
+                    r2 = 365 - (186 + ((m2 - 7) * 30) + d2);
+
+                }
+                r3 = (y2 - y1 - 1) * 365;
+                r = r1 + r2 + r3;
+                Console.WriteLine("" + r);
                 Console.ReadKey();
             }
         }
     }
 }
-
+    
